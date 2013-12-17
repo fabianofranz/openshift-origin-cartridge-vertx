fee0cb6cdfdbcdf3cfbf5b39803d73747c231d8b
fabianofranz <contact@fabianofranz.com>
2013-12-17 16:09:01 -0200

    Must be in repo dir to start vertex from server.js

--- versions/1.3/bin/run-vertx.sh
+++ versions/1.3/bin/run-vertx.sh
@@ -7,4 +7,6 @@ echo "VERTX_HOME: $VERTX_HOME"
 
 export TORQUEBOX_HOST="bam.keynote.projectodd.org"
 
+cd $OPENSHIFT_REPO_DIR
+
 $VERTX_HOME/bin/vertx run  ./server.js  -cluster -cluster-port 9123 -cluster-host ${env.OPENSHIFT_VERTX_IP} -advertise-port ${env.OPENSHIFT_VERTX_VERTX_PROXY_PORT} -advertise-host ${env.OPENSHIFT_VERTX_PROXY_IP}
